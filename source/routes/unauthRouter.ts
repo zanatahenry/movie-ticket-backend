1049470f533846b235d8274b2ac7c50c4e1f6e16
Henry Zanata <henryzanata@hotmail.com>
2024-07-31 22:47:41 -0300

    feat(signin-signup): added signin and signup, created authMiddle

--- source/routes/unauthRouter.ts
+++ source/routes/unauthRouter.ts
@@ -2,15 +2,17 @@ import cors from 'cors'
 
 import { NextFunction, Request, Response, Router } from "express";
 import SigninController from '../features/Signin/SigninController';
-import RegisterController from '../features/Register/RegisterController';
+import SignupController from '../features/Signup/SignupController';
 
 const unauthRouter = Router()
 
 unauthRouter.use(cors())
 
 unauthRouter.use('/signin', SigninController)
-unauthRouter.use('/register', RegisterController)
+unauthRouter.use('/signup', SignupController)
 
 unauthRouter.get('/status', (request: Request, response: Response, next: NextFunction) => {
   return response.status(200).send('available')
 })
+
+export default unauthRouter
